161c96247bcebad3f2837dff08d435a256d4b58c
Robert-Jan Keizer <robertjankeizer@gmail.com>
2016-04-24 12:07:02 +0200

    Chromecast play/pause on play/pause

--- Chromecast.py
+++ Chromecast.py
@@ -1,46 +1,40 @@
 from __future__ import print_function
-from gi.repository import RB, GObject, Peas, Gtk, Gio, Gdk
+
 import operator
-import webbrowser
+
 import pychromecast
-import urllib.parse, urllib.request
-import re
+from gi.repository import RB, GObject, Peas, Gtk
 
 
 class ChromecastSource(RB.StaticPlaylistSource):
     def __init__(self, **kwargs):
         super(ChromecastSource, self).__init__(kwargs)
         self.__activated = False
-        self.__playing_source = None
-        self.__playing_source_signals = []
+        self.__isChromecastConnect = False
+        self.__chromecast = None
+        self.__chromecast_player = None
+        self.__entry_view = None
         self.__signals = []
-        self.__filter = None
-        self.__source_is_lib = False
-        self.__song_count = 0
-        self.__update_in_progress = False
-        self.__queue_signal_id = None
 
     def setup(self):
         if self.__activated:
             return
-
-        print("ACTIVATING SOURCE!")
+        self.__chromecast = pychromecast.get_chromecast(friendly_name="RobsKamerMuziek")
+        self.__chromecast_player = self.__chromecast.media_controller
         self.plugin = self.props.plugin
         self.__activated = True
         self.__entry_view = self.get_entry_view()
-        self.__playing_source = None
         self.draw_sidebar()
         # self.setup_actions()
 
-        signals = self.__signals
         shell = self.get_property("shell")
         player = shell.get_property("shell-player")
-        signals.append((player.connect(
-            "playing-source-changed",
-            self.source_changed_callback),
-                        player))
 
-        # Activating Now Playing.
+        # Source change listener
+        # self.__signals.append((player.connect("playing-source-changed", self.source_changed_callback), player))
+
+        # PlayPause listener
+        self.__signals.append((player.connect("playing-changed", self.playing_changed_callback), player))
         playing_source = player.get_playing_source()
         # FIXME: Do not call the callback directly, move the common
         # code to a new method and call it in both places.
@@ -55,13 +49,12 @@ class ChromecastSource(RB.StaticPlaylistSource):
         iter = Gtk.TreeIter()
         if playing_entry and not model.entry_to_iter(playing_entry, iter):
             model.add_entry(playing_entry, 0)
-            signals.append((player.connect(
-                "playing-song-changed",
-                self.song_changed_callback,
-                False),
-                            player))
-            self.update_titles()
-
+            # signals.append((player.connect(
+            #     "playing-song-changed",
+            #     self.song_changed_callback,
+            #     False),
+            #                 player))
+            model.add_entry(["hallo", "Connected"], 1)
 
     def draw_sidebar(self):
         shell = self.get_property("shell")
@@ -94,7 +87,7 @@ class ChromecastSource(RB.StaticPlaylistSource):
                          True, True)
         sidebar.set_visible(True)
         sidebar.show_all()
-
+        # sidebar.
         # Connect to the "entry-activated" signal of the sidebar
         # sidebar.connect("entry-activated",
         #                 self.sidebar_entry_activated_callback)
@@ -104,16 +97,25 @@ class ChromecastSource(RB.StaticPlaylistSource):
     def cell_data_func(self, sidebar_column, renderer, tree_model, iter, data):
         db = self.get_property("shell").get_property("db")
         entry = tree_model.get(iter, 0)[0]
-        title = entry.get_string(RB.RhythmDBPropType.TITLE)
-        artist = entry.get_string(RB.RhythmDBPropType.ARTIST)
-        album = entry.get_string(RB.RhythmDBPropType.ALBUM)
+        title = entry[0]
+        album = entry[1]
         markup = "<span size=\"smaller\">" + \
                  "<b>" + GObject.markup_escape_text(title) + "</b>\n" + \
-                 "<i>" + GObject.markup_escape_text(album) + "</i>\n" + \
-                 "<i>" + GObject.markup_escape_text(artist) + "</i></span>"
+                 "<i>" + GObject.markup_escape_text(album) + "</i></span>"
         renderer.set_property("markup", markup)
 
-    
+    def playing_changed_callback(self, player, playing):
+        print("PLAY STATE CHANGED!")
+        if self.__chromecast_player.status.content_id is None:
+            self.__chromecast_player.play_media('http://pleer.com/browser-extension/files/545026i1uP.mp3', 'video/mp3')
+        if playing:
+            state = RB.EntryViewState.PLAYING
+            self.__chromecast_player.play()
+        else:
+            state = RB.EntryViewState.PAUSED
+            self.__chromecast_player.pause()
+
+
 # self.__chromecast = pychromecast.get_chromecast(friendly_name="RobsKamerMuziek")
 # self.__chromecast_player = self.__chromecast.media_controller
 
@@ -140,10 +142,10 @@ class Chromecast(GObject.Object, Peas.Activatable):
 
         self.__source.setup()
 
-
     def do_deactivate(self):
         # destroy source
         self.__source.do_delete_thyself()
         del self.__source
+
 
 GObject.type_register(ChromecastSource)

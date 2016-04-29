1ea463d1f17131ca4b7d607f4d1a87a84fe620be
Robert-Jan Keizer <robertjankeizer@gmail.com>
2016-04-29 16:42:33 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+f [ ! -d "~/.local/share/rhythmbox/plugins/" ]
+    mkdir -p ~/.local/share/rhythmbox/plugins/
+fi
 cd ~/.local/share/rhythmbox/plugins/
-if [ ! -d "./chromecast" ]
+f [ ! -d "./chromecast" ]
     then
     git clone https://github.com/KeizerDev/Rhythmbox-Chromecast.git chromecast
 fi
 cd chromecast/
+git pull origin master
 pip install -r requirements.txt

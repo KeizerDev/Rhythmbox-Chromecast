c037daa5e82539fb3563069df11e4a8b6552ff7c
vezza <vezza@inwind.it>
2022-09-04 12:08:49 +0200

    Error #16 - 'pychromecast' has no attribute 'get_chromecast' - workaround

--- Prefs.py
+++ Prefs.py
@@ -2,6 +2,6 @@
 # Follow their example to get your chromecastname
 #
 
-chromecastName = "RobsKamerMuziek"  # pychromecast.get_chromecasts_as_dict().keys()
-server = 'http://192.168.1.147:8000/' # replace this with your local ip and your port
-port = 8000
+chromecastName = "Google Chromecast"  # pychromecast.get_chromecasts_as_dict().keys()
+server = 'http://192.168.1.104:8050' # replace this with your local ip and your port
+port = 8050
